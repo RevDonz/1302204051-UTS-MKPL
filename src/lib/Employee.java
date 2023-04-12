5986b5df07b66d49804f50120ee9f7e7dad137fb
RevDonz <aprilio842@gmail.com>
2023-04-12 13:44:27 +0700

    Mengubah tipe data gender dari boolean ke enum

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -6,6 +6,9 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Employee {
+  public enum Gender {
+    MALE, FEMALE
+  }
 
 	private String employeeId;
 	private String firstName;
@@ -19,7 +22,7 @@ public class Employee {
 	private int monthWorkingInYear;
 	
 	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
+	private Gender gender;
 	
 	private int monthlySalary;
 	private int otherMonthlyIncome;
@@ -31,7 +34,7 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, Gender gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
