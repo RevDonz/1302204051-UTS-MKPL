d08b8dde513302d0dea19b90d6aad972d7f03241
RevDonz <aprilio842@gmail.com>
2023-04-12 21:18:54 +0700

    Membuat class Person

---
+++ src/lib/Person.java
@@ -0,0 +1,82 @@
+package lib;
+
+public class Person {
+    public enum Gender {
+        MALE, FEMALE
+    }
+
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String idNumber;
+    private String address;
+    private Gender gender;
+    private boolean isForeigner;
+    
+    public Person(String employeeId, String firstName, String lastName, String idNumber, String address, Gender gender, boolean isForeigner) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+        this.address = address;
+        this.gender = gender;
+        this.isForeigner = isForeigner;
+    }
+    
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public boolean isIsForeigner() {
+        return isForeigner;
+    }
+
+    public void setIsForeigner(boolean isForeigner) {
+        this.isForeigner = isForeigner;
+    }
+    
+}
